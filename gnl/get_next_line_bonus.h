f2f15cd86a1b5c1c938d95c721b4ea8c5a5c087a
Eunseong Hong <euhong@c8r1s2.42seoul.kr>
2021-05-11 14:06:33 +0900

    gnl start

---
+++ gnl/get_next_line_bonus.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: euhong <euhong@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/11 11:01:43 by euhong            #+#    #+#             */
+/*   Updated: 2021/05/11 11:26:22 by euhong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+# define BUFFER_SIZE 1024
+
+int	get_next_line(int fd, char **line);
+
+#endif
