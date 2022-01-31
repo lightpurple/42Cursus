93ed470d4441b105505fc48d99448cac32ba6ac8
lightpurple <dmstjd10@gmail.com>
2022-01-31 23:53:32 +0900

    22.1.31 mlx study

--- solong/mlx.c
+++ solong/mlx.c
@@ -13,14 +13,31 @@ int key_exit(int keycode)
 	return (0);
 }
 
+// int stay_player(void *img, void *mlx, void *mlx_win)
+// {
+// 	int i = 0;
+// 	while (i < 3)
+// 	{
+// 		if (i == 3)
+// 			i = 0;
+// 		else
+// 		{
+// 			img = mlx_xpm_file_to_image(mlx, "./image/tile.xpm", 0, 0);
+// 			// mlx_put_image_to_window(mlx, mlx_win, img, j * SIZE, i * SIZE);
+// 		}
+// 	}
+// }
+
 void mlx_start(t_map map)
 {
 	void *mlx;
 	void *mlx_win;
 	t_data img;
-	int width;
-	int height;
+	int *width;
+	int *height;
 
+	*width = 50;
+	*height = 50;
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, map.width * SIZE, map.height * SIZE, "so_long");
 
@@ -36,17 +53,31 @@ void mlx_start(t_map map)
 		j = 0;
 		while (j < map.width)
 		{
-			if (map.map[i][j] == 1)
+			if (map.map[i][j] == '1')
 			{
 				img.img = mlx_xpm_file_to_image(
-					mlx, "./image/wall.xpm", &width, &height);
+					mlx, "./image/wall.xpm", width, height);
+				mlx_put_image_to_window(
+					mlx, mlx_win, img.img, j * SIZE, i * SIZE);
+			}
+			else if (map.map[i][j] == '0')
+			{
+				img.img = mlx_xpm_file_to_image(
+					mlx, "./image/tile.xpm", width, height);
+				mlx_put_image_to_window(
+					mlx, mlx_win, img.img, j * SIZE, i * SIZE);
+			}
+			else if (map.map[i][j] == 'P')
+			{
+				img.img = mlx_xpm_file_to_image(
+					mlx, "./image/mush/mush0.xpm", width, height);
 				mlx_put_image_to_window(
 					mlx, mlx_win, img.img, j * SIZE, i * SIZE);
 			}
 			else
 			{
 				img.img = mlx_xpm_file_to_image(
-					mlx, "./image/tile.xpm", &width, &height);
+					mlx, "./image/coin/coin0.xpm", width, height);
 				mlx_put_image_to_window(
 					mlx, mlx_win, img.img, j * SIZE, i * SIZE);
 			}
@@ -55,5 +86,6 @@ void mlx_start(t_map map)
 		i++;
 	}
 	//////////////
+	// mlx_loop_hook(mlx, stay_player, img.img);
 	mlx_loop(mlx);
 }
