218162d4e2ac7998f0ae3d178f0486e1716b42c1
lightpurple <dmstjd10@gmail.com>
2022-03-31 14:22:34 +0900

    pushswap day finish

--- push_swap/get_next_line.h
+++ push_swap/get_next_line.h
@@ -6,29 +6,29 @@
 /*   By: euhong <euhong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/11 11:01:43 by euhong            #+#    #+#             */
-/*   Updated: 2022/01/24 22:11:53 by euhong           ###   ########.fr       */
+/*   Updated: 2022/03/31 14:22:24 by euhong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include <stdlib.h>
-#include <unistd.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 1024
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
 
-#ifndef OPEN_MAX
-#define OPEN_MAX 256
-#endif
+# ifndef OPEN_MAX
+#  define OPEN_MAX 256
+# endif
 
-int get_next_line(int fd, char **line);
-int ft_strlen(char *str);
-int ft_find_newline(char *str);
-char *ft_strdup(char *s1);
-char *ft_strncpy(char *dst, char *src, int loc);
-char *ft_strncat(char *dst, char *src, int len);
+int		get_next_line(int fd, char **line);
+int		ft_strlen(char *str);
+int		ft_find_newline(char *str);
+char	*ft_strdup(char *s1);
+char	*ft_strncpy(char *dst, char *src, int loc);
+char	*ft_strncat(char *dst, char *src, int len);
 
 #endif
